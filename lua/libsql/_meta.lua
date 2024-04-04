f20a7ef5c6c7aa925ff751e485ce603e199cc849
Will Hopkins <willothyh@gmail.com>
2024-04-03 23:50:49 -0700

    refactor: move types to meta file

---
+++ lua/libsql/_meta.lua
@@ -0,0 +1,57 @@
+---@meta
+
+---@class libsql.Connection
+local Connection = {}
+
+---@param query string
+---@param parameters any[]
+---@return libsql.Rows
+function Connection:query(query, parameters) end
+
+---@param query string
+---@param parameters any[]
+---@return integer
+function Connection:execute(query, parameters) end
+
+---@class libsql.Rows
+local Rows = {}
+
+---@return libsql.Row?
+function Rows:next() end
+
+---@return integer
+function Rows:column_count() end
+
+---@param index integer
+---@return string
+function Rows:column_name(index) end
+
+---@param index integer
+---@return string
+function Rows:column_type(index) end
+
+---@class libsql.Row
+local Row = {}
+
+---@param index integer
+---@return any
+function Row:get(index) end
+
+---@return integer
+function Row:column_count() end
+
+---@param index integer
+---@return string
+function Row:column_name(index) end
+
+---@param index integer
+---@return string
+function Row:column_type(index) end
+
+---@class libsql.Lib
+local LibSQL = {}
+
+---@param uri string
+---@param token string
+---@return libsql.Connection
+function LibSQL.connect(uri, token) end
--- lua/libsql/init.lua
+++ lua/libsql/init.lua
@@ -1,21 +1,4 @@
----@class libsql
----@field connect fun(uri: string, token: string): libsql.Connection
+---@type libsql.Lib
 local M = require("libsql.native")
-
----@class libsql.Rows
----@field next fun(self: libsql.Rows): libsql.Row?
----@field column_count fun(self: libsql.Rows): integer
----@field column_name fun(self: libsql.Rows, index: integer): string
----@field column_type fun(self: libsql.Rows, index: integer): string
-
----@class libsql.Row
----@field get fun(self: libsql.Row, index: integer): any
----@field column_count fun(self: libsql.Row): integer
----@field column_name fun(self: libsql.Row, index: integer): string
----@field column_type fun(self: libsql.Row, index: integer): string
-
----@class libsql.Connection
----@field query fun(self: libsql.Connection, query: string, parameters: any[]): libsql.Rows
----@field execute fun(self: libsql.Connection, query: string, parameters: any[]): integer
 
 return M
